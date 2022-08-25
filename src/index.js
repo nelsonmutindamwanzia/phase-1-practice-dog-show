d75bd3e05bf195e421db3d9010bd12e5a5a77cb2
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-25 08:55:08 +0300

    renders list of dogs to table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    function renderDog(dog){
+        let trow = document.createElement('tr')
+        trow.innerHTML = `
+            <td>${dog.name}</td>
+            <td>${dog.breed}</td>
+            <td>${dog.sex}</td>
+            <td><button>Edit</button></td>
+        `
+        trow.querySelector('button').addEventListener('click',(e)=>{
+            console.log('clicked');
+        })
+        
+        document.querySelector('#table-body').appendChild(trow)
+  
+    }
+    
+    function fetchDog(){
+        fetch('http://localhost:3000/dogs')
+        .then(resp => resp.json())
+        .then(dogData => dogData.forEach(element => {
+            renderDog(element)
+        }))
+    }
+
+    fetchDog()
+
 })
